5e68519e0b7fa30a2136ff1e6306752edcc21c76
Mikha38 <oyks1379@yandex.ru>
2023-08-02 17:15:54 +0300

    Правки

--- components/sections/05Partners.tsx
+++ components/sections/05Partners.tsx
@@ -10,24 +10,24 @@ export default function Partners(){
     return(
         <section id="05Partners" className="_section pt-[40px] md:pt-[100px]">
             <div className="_wrapper">
-                <h2 className={`${BEBAS_NEUE} _h `}>
+                <h2 data-aos="fade-right" data-aos-duration="1500" className={`${BEBAS_NEUE} _h `}>
                     OUR <span className="text-_green">PARTNERS</span>
                 </h2>
                 <div className="grid grid-cols-6 sm:grid-cols-5 mt-10">
                     <div className="col-span-2 sm:col-span-1 flex justify-center">
-                        <Image src={first} alt="" className="block"/>
+                        <Image data-aos="zoom-in" data-aos-duration="1500" src={first} alt="" className="block"/>
                     </div>
                     <div className="col-span-2 sm:col-span-1 flex justify-center">
-                        <Image src={second} alt="" className="block"/>
+                        <Image data-aos="zoom-in" data-aos-duration="1500"  src={second} alt="" className="block"/>
                     </div>
                     <div className="col-span-2 sm:col-span-1 flex justify-center">
-                        <Image src={third} alt="" className="block"/>
+                        <Image data-aos="zoom-in" data-aos-duration="1500"  src={third} alt="" className="block"/>
                     </div>
                     <div className="col-span-3 sm:col-span-1 flex md:justify-center justify-end w-full pr-[10%]">
-                        <Image src={fourth} alt="" className="block"/>
+                        <Image data-aos="zoom-in" data-aos-duration="1500"  src={fourth} alt="" className="block"/>
                     </div>
                     <div className="col-span-3 sm:col-span-1 flex md:justify-center justify-start w-full pl-[10%]">
-                        <Image src={fifth} alt="" className="block"/>
+                        <Image data-aos="zoom-in" data-aos-duration="1500"  src={fifth} alt="" className="block"/>
                     </div>
                 </div>
             </div>
